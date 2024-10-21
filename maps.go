d7a55a7a72c7be0c88b584e74695ad90e4ff9bba
Benita Emudianughe <emusbeny@gmail.com>
2024-10-21 22:10:25 +0100

    Initializing Maps

---
+++ maps.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// INITIALIZING MAPS
+	heights := map[string]int{
+		"Joan":   168,
+		"Benita": 169, //take note of comma at end
+	}
+	// OR
+	lengths = make(map[string]int)
+
+	lengths["Toyin_land"] = 750
+}
