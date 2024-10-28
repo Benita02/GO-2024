ce82d19a66871bc60b1f71c843e6aec96c9d29ca
Benita Emudianughe <emusbeny@gmail.com>
2024-10-28 12:29:15 +0100

    ädd several struct and created the shape interface to host the Area() method then created a calculateArea function that takes in a slice of type []Shape(he shape interface, so it'll be easy to calculate areas of any other new shapes that might arise)

--- interfaces.go
+++ interfaces.go
@@ -19,6 +19,40 @@ func (ds DigitString) CountOddEven() (int, int) {
 	}
 	return odds, evens
 }
+
+type Shape interface {
+	Area() float64
+}
+type Circle struct {
+	radius float64
+}
+type Square struct {
+	length float64
+}
+
+// Circle implements Shape
+func (c Circle) Area() float64 {
+	return 3.14 * c.radius * c.radius
+}
+
+// Square implements Shape
+func (s Square) Area() float64 {
+	return s.length * s.length
+}
+func calculateArea(listOfShapes []Shape) {
+	for _, shape := range listOfShapes {
+		fmt.Println("Area: ", shape.Area())
+	}
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.base * t.height
+}
 func main() {
 	// IMPLEMENTING AN INTERFACE
 	s := DigitString("123456789")
@@ -30,6 +64,19 @@ func main() {
 	d = s
 	fmt.Println(d.CountOddEven())
 	// HOW I MAY USE INTERFACES
+	c1 := Circle{radius: 5}
+	s1 := Square{length: 6}
+
+	shapes := []Shape{c1, s1}
+	calculateArea(shapes)
+	/*
+	   Suppose you now have a new shape, Triangle, that you want to add to your program. In this case, you just need to define the struct for the Triangle shape and
+	   implement the Shape interface by providing the implementation for the Area()
+	   function:*/
+	t1 := Triangle{base: 6, height: 8}
+	shapes = []Shape{c1, s1, t1}
+	calculateArea(shapes)
+
 	//Stringer interface in fmt.Println, check go docs to better understand
 	//IMPLEMENTING MULTIPLE INTERFACES
 
