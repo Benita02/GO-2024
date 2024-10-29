99031f19e6064e827bdfa2976bc77f17ef8d6bf5
Benita Emudianughe <emusbeny@gmail.com>
2024-10-29 22:04:28 +0100

    deleted simple say function and worked with credit() and debit() functions are updating a shared variable balance

--- goRoutines.go
+++ goRoutines.go
@@ -1,10 +1,45 @@
-// THREADING AND GO ROUTINES
+ // THREADING AND GO ROUTINES
 package main
+import (
+ "fmt"
+ "math/rand"
+ "time"
+)
 
-import "fmt"
+var balance int
+
+func credit() {
+ for i := 0; i < 5; i++ {
+     balance += 100
+     time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+     fmt.Println("After crediting, balance is", balance)
+ }
+}
+
+func debit() {
+ for i := 0; i < 5; i++ {
+     balance -= 100
+     time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+     fmt.Println("After debiting, balance is", balance)
+ }
+}
 
 func main() {
-	go say("Hello", 3)
-	go say("World", 2)
-	fmt.Scanln() // fmt.Scanln() ensures the program doesn't terminate immediately
+ balance = 200
+ fmt.Println("Initial balance is", balance)
+ go credit()
+ go debit()
+
+ fmt.Scanln() // fmt.Scanln() ensures the program doesn't terminate immediately
+/*
+Issues with Shared Resources
+When credit() and debit() run concurrently, unpredictable results occur because they both access balance without waiting for each other. This is known as a race condition. In the output, you’ll see inconsistent results like a balance of 200 after crediting when it should be 300, or other errors in sequence.*/
+    
+
+
+
+
+    
+}
+
 }
