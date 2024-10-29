6b469d8382c506272f7b6b319bbfef252f4c82cd
Benita Emudianughe <emusbeny@gmail.com>
2024-10-29 21:49:05 +0100

    practiced goroutines with simple say function

--- goRoutines.go
+++ goRoutines.go
@@ -1,1 +1,10 @@
-// GO ROUTINES
+// THREADING AND GO ROUTINES
+package main
+
+import "fmt"
+
+func main() {
+	go say("Hello", 3)
+	go say("World", 2)
+	fmt.Scanln() // fmt.Scanln() ensures the program doesn't terminate immediately
+}
