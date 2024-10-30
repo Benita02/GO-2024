89f00a104e226ac5fd1869b2ac227f14a016fddd
Benita Emudianughe <emusbeny@gmail.com>
2024-10-30 15:09:46 +0100

    Replaced Mutex locks with atomic counters to handle concurrent updates to shared variables. Using tomic.AddInt64() provides a more efficient, lightweight solution for simple increments/decrements, improving performance by reducing lock contention. This change ensures safe concurrent access while simplifying the code.

--- goRoutines.go
+++ goRoutines.go
@@ -5,38 +5,55 @@ import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var (
-	balance int
-	mu      sync.Mutex
+	balance int64      //changed type to int64 bcos of use of atomic counters
+	mu      sync.Mutex //understand better when to use &sync.Mutex{} instead
 )
 
+// func credit() {
+// 	for i := 0; i < 5; i++ {
+// 		mu.Lock() // Lock the mutex before accessing balance
+// 		balance += 100
+// 		mu.Unlock() // Unlock it after updating
+// 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+// 		fmt.Println("After crediting, balance is", balance)
+// 	}
+// }
+
+// func debit() {
+// 	for i := 0; i < 5; i++ {
+// 		mu.Lock() // Lock before accessing balance
+// 		balance -= 100
+// 		mu.Unlock() // Unlock after updating
+// 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+// 		fmt.Println("After debiting, balance is", balance)
+// 	}
+// }
+
+// USING ATOMIC COUNTERS
 func credit() {
-	for i := 0; i < 5; i++ {
-		mu.Lock() // Lock the mutex before accessing balance
-		balance += 100
-		mu.Unlock() // Unlock it after updating
+	for i := 0; i < 10; i++ {
+		// adds 100 to balance atomically
+		atomic.AddInt64(&balance, 100)
 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-		fmt.Println("After crediting, balance is", balance)
 	}
 }
-
 func debit() {
 	for i := 0; i < 5; i++ {
-		mu.Lock() // Lock before accessing balance
-		balance -= 100
-		mu.Unlock() // Unlock after updating
+		// deducts -100 from balance atomically
+		atomic.AddInt64(&balance, -100)
 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-		fmt.Println("After debiting, balance is", balance)
 	}
 }
-
 func main() {
 	balance = 200
 	fmt.Println("Initial balance is", balance)
 	go credit()
 	go debit()
 	fmt.Scanln()
+	fmt.Println("Final balance:", balance)
 }
