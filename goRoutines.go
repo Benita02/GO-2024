16051d48d87a62b0cb8ec7272b4b1503ed17f8ca
Benita Emudianughe <emusbeny@gmail.com>
2024-10-31 10:44:58 +0100

    To print out the interim balance in the goroutines, we use the Mutex object to mark out the critical section as done earlier

--- goRoutines.go
+++ goRoutines.go
@@ -10,8 +10,8 @@ import (
 )
 
 var (
-	balance int64      //changed type to int64 bcos of use of atomic counters
-	mu      sync.Mutex //understand better when to use &sync.Mutex{} instead
+	balance int64           //changed type to int64 bcos of use of atomic counters
+	mu      = &sync.Mutex{} //understand better when to use &sync.Mutex{} instead
 )
 
 // func credit() {
@@ -35,25 +35,41 @@ var (
 // }
 
 // USING ATOMIC COUNTERS
-func credit() {
+func credit(wg *sync.WaitGroup) {
+	// notify the WaitGroup when we are done
+	defer wg.Done()
 	for i := 0; i < 10; i++ {
+		mu.Lock()
 		// adds 100 to balance atomically
 		atomic.AddInt64(&balance, 100)
+		fmt.Println("After crediting, balance is", balance)
+		mu.Unlock()
 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	}
 }
-func debit() {
+func debit(wg *sync.WaitGroup) {
+	// notify the WaitGroup when we are done
+	defer wg.Done()
 	for i := 0; i < 5; i++ {
+		mu.Lock()
 		// deducts -100 from balance atomically
 		atomic.AddInt64(&balance, -100)
+		fmt.Println("After debiting, balance is", balance)
+		mu.Unlock()
 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	}
 }
 func main() {
+	var wg sync.WaitGroup //why didn't we declare this as a global variable
 	balance = 200
 	fmt.Println("Initial balance is", balance)
-	go credit()
-	go debit()
-	fmt.Scanln()
+
+	wg.Add(1)
+	go credit(&wg) //I don't fully understand why we're passing the waitgroup address
+
+	wg.Add(1)
+	go debit(&wg)
+
+	wg.Wait()
 	fmt.Println("Final balance:", balance)
 }
