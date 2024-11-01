421e1493d6be573a826ae3e79904e363478f3bb1
Benita Emudianughe <emusbeny@gmail.com>
2024-11-01 05:01:39 +0100

    I’ll split this slice into five parts, take each part and pass it to the sum() function together with the channel c, and call it as a goroutine

--- channels.go
+++ channels.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
 func main() {
 	//To create a channel
 	ch := make(chan int)
@@ -11,5 +21,19 @@ func main() {
 		fmt.Println(value)
 	}()
 	ch <- 5
+
+	s := []int{}
+	sliceSize := 10
+	for i := 0; i < sliceSize; i++ {
+		s = append(s, rand.Intn(100))
+	}
+	c := make(chan int)
+	partSize := 2
+	parts := sliceSize / partSize
+	i := 0
+	for i < parts {
+		go sum(s[i*partSize:(i+1)*partSize], c)
+		i += 1
+	}
 
 }
