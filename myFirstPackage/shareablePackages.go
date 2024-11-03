f3c6f350715393ae9fd4e698c5c8a6ba81368176
Benita Emudianughe <emusbeny@gmail.com>
2024-11-03 19:57:01 +0100

    No electricity available so I used my phone to continue learning about packages

--- myFirstPackage/shareablePackages.go
+++ myFirstPackage/shareablePackages.go
@@ -1,1 +1,5 @@
-//
+/*To create a shareable package,
+first use the go env command
+to find out the location of the GOPATH environment variable*/
+
+// No electricity available so I used my phone to continue learning about packages
