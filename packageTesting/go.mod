f4fa122a2a22b61fac253ae51c01a744f5f650d7
Benita Emudianughe <emusbeny@gmail.com>
2024-11-07 16:26:20 +0100

    Worked with third party package - turtle, aim of package is to take in string name of the emoji and it returns the said emoji in the terminal

--- packageTesting/go.mod
+++ packageTesting/go.mod
@@ -1,3 +1,5 @@
 module packageTesting
 
 go 1.23.2
+
+require github.com/hackebrot/turtle v0.2.0
--- packageTesting/thirdPartyPackages.go
+++ packageTesting/thirdPartyPackages.go
@@ -1,1 +1,17 @@
-//Working with third party packages
+// Working with third party packages
+package main
+
+import (
+	"fmt"
+
+	"github.com/hackebrot/turtle"
+)
+
+func main() {
+	emoji, ok := turtle.Emojis["middle finger"]
+	if !ok {
+		fmt.Println("No emoji found.")
+	} else {
+		fmt.Println("Fuck you bitches", emoji.Char)
+	}
+}
