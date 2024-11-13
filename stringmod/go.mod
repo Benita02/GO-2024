6cb817513183f4f708ca176ff8e70e3021eb3b93
Benita Emudianughe <emusbeny@gmail.com>
2024-11-13 21:15:48 +0100

    learnt about using the cmd line with various commands and flags to better understand how to use a third party package/module and more about web API's in GO

--- stringmod/go.mod
+++ stringmod/go.mod
@@ -3,3 +3,9 @@ module github.com/Benita02/GO-2024.git
 go 1.23.2
 
 require github.com/hackebrot/turtle v0.2.0
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+)
--- stringmod/main.go
+++ stringmod/main.go
@@ -10,5 +10,7 @@ import (
 func main() {
 	o, e := strings.CountOddEven("12345")
 	fmt.Println(o, e) // 3 2
-	fmt.Println(quotes.GetEmoji("heart"))
+	fmt.Println(quotes.GetEmoji("bat"))
 }
+
+// learnt about using the cmd line with various commands and flags to better understand how to use a third party package/module and more about web API's in GO
